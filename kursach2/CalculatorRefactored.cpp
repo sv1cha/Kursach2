1c954e6e2c1fd19295abc6e1de63091ee8133f92
sv1cha <metrasverch28@mail.ru>
2024-11-20 21:58:09 +0300

    Update CalculatorRefactored.cpp

--- kursach2/CalculatorRefactored.cpp
+++ kursach2/CalculatorRefactored.cpp
@@ -1,24 +1,24 @@
 #include "CalculatorRefactored.h"
 
-MathEngine::MathEngine(std::vector<int64_t> data)
-{
-    try {
-        computed_value = 0;
-        for(auto num : data) {
-            computed_value += num * num;
-            if (computed_value > INT64_MAX / 2) {
-                computed_value = INT64_MAX;
-            } else if (computed_value < INT64_MIN / 2) {
-                computed_value = INT64_MIN;
-            }
+MathEngine::MathEngine(std::vector<int64_t> data) {
+    computed_value = 0;
+    for (auto num : data) {
+        int64_t square;
+        
+        // Check for potential overflow
+        if (num > 0 && num > (INT64_MAX - computed_value) / num) {
+            computed_value = INT64_MAX;
+            return;
+        } else if (num < 0 && num < (INT64_MIN - computed_value) / num) {
+            computed_value = INT64_MIN;
+            return;
         }
-    } catch (boost::numeric::negative_overflow& e) {
-        computed_value = std::numeric_limits<int64_t>::lowest();
-    } catch (boost::numeric::positive_overflow& e) {
-        computed_value = std::numeric_limits<int64_t>::max();
+
+        // Safe to calculate
+        square = num * num;
+        computed_value += square;
     }
 }
 
 int64_t MathEngine::retrieve_result() {
     return computed_value;
-}
